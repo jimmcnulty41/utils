4768b2203f0b10d21db1221f9fc1ac9890a1bcb8
James McNulty <jamesm@extrahop.com>
2018-02-05 11:34:57 -0800

    cleaner zsh stuff

--- zshStuff/do.sh
+++ zshStuff/do.sh
@@ -2,6 +2,8 @@
 
 git pull
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s $(which zsh)
+ln -s $(pwd) ~/.oh-my-zsh/themes/agnoster-fcamblor.zsh-theme
 sudo apt-get install fonts-powerline
 cp .zshrc ~/.zshrc
 
