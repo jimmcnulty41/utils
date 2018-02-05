bd7912c176930ab92da25e309e0a9e293055e49d
James McNulty <jimmcnulty41@gmail.com>
2018-02-05 11:16:07 -0800

    update do

--- zshStuff/do.sh
+++ zshStuff/do.sh
@@ -2,7 +2,7 @@
 
 git pull
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sh ./oh-my-zsh-agnoster-fcamblor/install
+sudo apt-get install fonts-powerline
 cp .zshrc ~/.zshrc
 
 
