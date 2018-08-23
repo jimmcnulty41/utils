ac63b95dcddf4e76bfc0f42ec59166aaa0c0e9b4
James McNulty <jimmcnulty41@gmail.com>
2018-08-22 21:18:50 -0700

    Add more aliases

--- gitstuff/gitaliases.sh
+++ gitstuff/gitaliases.sh
@@ -3,5 +3,6 @@ git config --global alias.co checkout
 git config --global alias.s status
 git config --global alias.branch-name "!git rev-parse --abbrev-ref HEAD"
 git config --global alias.pushu "!git push --set-upstream origin \$(git branch-name)"
+git config --global alias.stat "!git log; git status"
 
 
